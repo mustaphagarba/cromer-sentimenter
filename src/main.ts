b556b6b13253b3a1ede498fcd92401917e10003b
Mustapha Muhammad Garba <mgarba88@gmail.com>
2018-07-30 01:22:37 +0100

    Initial scaffolding

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import * as path from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', __dirname + '/views');
+    // this sets ejs as the view engine. Remember this!
+  app.set('view engine', 'ejs');
+
+  await app.listen(3000);
+}
+bootstrap();
